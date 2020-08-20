cf346177bccd64230ae7bc26db200cdd9d7aee15
Tim McIver <tim.mciver@ontellus.com>
2020-08-20 17:53:32 -0400

    Some code to motivate bind.

---
+++ user.hs
@@ -0,0 +1,15 @@
+data User = User { name :: String
+                 , age :: Int
+                 } deriving (Show)
+
+makeUser :: String -> Int -> Either String User
+makeUser name age = if (age < 18)
+                    then (Left "Not old enough")
+                    else if (length name < 3)
+                         then (Left "Name too short")
+                         else Right (User name age)
+
+businessLogic :: User -> Either String Bool
+businessLogic (User _ age) = if (age > 75)
+                             then (Left "Too old to do this")
+                             else (Right True)
